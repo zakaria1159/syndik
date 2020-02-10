5d62de4807595c5102f72cbf8754de235b38ceac
Zakaria <z.boumarouane@gmail.com>
2020-02-10 20:06:08 +0100

    add login page

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListTile(
           title: Text(record.name),
           trailing: Text(record.votes.toString()),
-          onTap: () => print(record),
+            onTap: () => record.reference.updateData({'votes': FieldValue.increment(1)})
         ),
       ),
     );
